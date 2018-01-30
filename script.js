d17f9b962a331376846c9cb38c84d14738446cff
David Stout <davidgstout63@gmail.com>
2018-01-30 12:58:17 -0500

    Created HTML structure, and limited incrementing functionality for player 1 button.

---
+++ script.js
@@ -0,0 +1,10 @@
+var p1Button = document.querySelector("#p1");
+var p2Button = document.getElementById('p2');
+var h1 = document.querySelector("h1");
+var p1Score = 0;
+
+p1Button.addEventListener("click", function (){
+	p1Score++;
+	console.log(p1Score);
+	h1.textContent = p1Score;
+	});
